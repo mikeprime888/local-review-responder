8ca0d3e71b621347330983295a823285cd908298
Mike Prime <mikeprime888@gmail.com>
2026-02-04 09:03:12 -0500

    Update Stripe API version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe';
 
 // Initialize Stripe
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2026-01-28.clover',
 });
 
 // Pricing configuration
