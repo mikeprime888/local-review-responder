b5feab7dd61240f8c4f344e9dcc7d1cea357a825
Mike Prime <mikeprime888@gmail.com>
2026-02-11 06:30:26 -0500

    Add container padding to Reviews page

--- src/app/dashboard/reviews/page.tsx
+++ src/app/dashboard/reviews/page.tsx
@@ -222,7 +222,7 @@ function ReviewsContent() {
   }
 
 return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+    <div>
       {/* Header */}
       <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-3 mb-6">
         <div>
